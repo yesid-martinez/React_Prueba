078e3b879a4e951ae2c86848a6516647bfabdf7c
DevGoat <121673430+yesid-martinez@users.noreply.github.com>
2026-04-19 04:45:44 +0000

    calculator

--- my-app/src/Componets/Boton.jsx
+++ my-app/src/Componets/Boton.jsx
@@ -1,8 +1,8 @@
 import './Boton.css'
 
 export const Boton = (params)=>{
-    const {texto, clase} = params //Desgloso params en texto y clase que se desestructuran en params
+    const {texto, clase, handleClick} = params //Desgloso params en texto y clase que se desestructuran en params
     return(
-        <button className={clase}>{texto}</button>
+        <button className={clase} onClick={handleClick}>{texto}</button>
     )
 }
--- my-app/src/Componets/Calculadora.jsx
+++ my-app/src/Componets/Calculadora.jsx
@@ -1,31 +1,46 @@
 import { Boton } from './Boton'
 import './Calculadora.css'
+import { useState } from 'react'
 
 export const Calculadora = ()=>{
+    //Agrego hook para cambiar valor de los botones
+    const [data, setData] = useState({operacion:'', resultado: ''});
+    
+    const escritura = (event)=>{
+        if(data.operacion.length >= 10) return
+        setData({...data, operacion: data.operacion + event.target.innerText})
+    }
+    const eliminar = ()=>{
+        setData({...data, operacion: data.operacion.slice(0, data.operacion.length - 1)})
+    }
+    const resultado = ()=>{
+        const resultado = eval(data.operacion)
+        setData({...data, resultado})
+    }
     return(
         <index>
-            <input className='resultado' type="text" value='50' />
-            <input className='display' type="text" value='25+25' />
+            <span className='resultado'>{data.resultado}</span>
+            <span className='display'>{data.operacion}</span>
             <Boton texto='C' clase='gris'/>
             <Boton texto='+/-' clase='gris'/>
             <Boton texto='%' clase='gris'/>
-            <Boton texto='/' clase='operacion'/>
-            <Boton texto='7' clase='numero'/>
-            <Boton texto='8' clase='numero'/>
-            <Boton texto='9' clase='numero'/>
-            <Boton texto='X' clase='operacion'/>
-            <Boton texto='4' clase='numero'/>
-            <Boton texto='5' clase='numero'/>
-            <Boton texto='6' clase='numero'/>
-            <Boton texto='-' clase='operacion'/>
-            <Boton texto='1' clase='numero'/>
-            <Boton texto='2' clase='numero'/>
-            <Boton texto='3' clase='numero'/>
-            <Boton texto='+' clase='operacion'/>
+            <Boton texto='/' clase='operacion' handleClick={escritura}/>
+            <Boton texto='7' clase='numero' handleClick={escritura}/>
+            <Boton texto='8' clase='numero' handleClick={escritura}/>
+            <Boton texto='9' clase='numero' handleClick={escritura}/>
+            <Boton texto='X' clase='operacion' handleClick={escritura}/>
+            <Boton texto='4' clase='numero' handleClick={escritura}/>
+            <Boton texto='5' clase='numero' handleClick={escritura}/>
+            <Boton texto='6' clase='numero' handleClick={escritura}/>
+            <Boton texto='-' clase='operacion' handleClick={escritura}/>
+            <Boton texto='1' clase='numero' handleClick={escritura}/>
+            <Boton texto='2' clase='numero' handleClick={escritura}/>
+            <Boton texto='3' clase='numero' handleClick={escritura}/>
+            <Boton texto='+' clase='operacion' handleClick={escritura}/>
             <Boton texto='.' clase=''/>
-            <Boton texto='0' clase=''/>
-            <Boton texto='<' clase=''/>
-            <Boton texto='=' clase='operacion'/>
+            <Boton texto='0' clase='' handleClick={escritura}/>
+            <Boton texto='<' clase='' handleClick={escritura}/>
+            <Boton texto='=' clase='operacion' handleClick={resultado}/>
         </index>
     ) 
 }
